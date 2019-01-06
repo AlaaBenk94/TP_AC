e1c867dbe6b6689f9ace3d543a1cea140d1e423c
AlaaBenk94 <a.benkarrad@gmail.com>
2019-01-06 04:49:47 +0100

    Proposition

--- src/deuxieme_partie/Historique.java
+++ src/deuxieme_partie/Historique.java
@@ -1,7 +1,15 @@
 package deuxieme_partie;
 
+import java.util.HashMap;
+
 public class Historique {
 
+    private HashMap<String, int[]> history;
 
+    public Historique(){
+        history = new HashMap<>();
+    }
+
+    public void addToHistoy(String)
 
 }
---
+++ src/deuxieme_partie/Proposition.java
@@ -0,0 +1,15 @@
+package deuxieme_partie;
+
+public class Proposition {
+
+    public String prop;
+    public int b;
+    public int m;
+
+    public Proposition(String prop){
+        prop = prop;
+        b = 0;
+        m = 0;
+    }
+
+}
